fb9e0c7dc3de8d4cde79ee4b369475cdccc8c340
Sam Cutmore <Sam.C279@gmail.com>
2025-07-25 13:26:54 +0100

    Add init py files for packages. Create template test file for parser. Will create task then branch for creating parser. Project setup should be done now.

---
+++ tests/TestGitHubURLParser.py
@@ -0,0 +1,11 @@
+import unittest
+
+class TestGitHubURLParser(unittest.TestCase):
+    def test_valid_urls(self):
+        pass
+
+    def test_invalid_url(self):
+        pass
+
+if __name__ == "__main__":
+    unittest.main()
